df2618f8438166d2dee391ad2721c2188b311b2d
titocava <titocava@gmail.com>
2024-08-23 23:28:09 -0300

    nuevo main tarea

--- PrimerProjecto.java/src/ar/edu/unlu/Poo1/MainTarea.java
+++ PrimerProjecto.java/src/ar/edu/unlu/Poo1/MainTarea.java
@@ -4,20 +4,28 @@ package ar.edu.unlu.Poo1;
 
 	import java.time.LocalDate;
 
+	import java.time.LocalDate;
+
 	public class MainTarea {
 	    public static void main(String[] args) {
+	        // Crear la lista de tareas
+	        Lista listaTareas = new Lista();
+
 	        // Crear tareas
 	        Tarea tarea1 = new Tarea("Ir al supermercado mañana", 1, LocalDate.now().plusDays(1));
 	        Tarea tarea2 = new Tarea("Consultar repuesto del auto", 2, LocalDate.now().minusDays(1));
 	        Tarea tarea3 = new Tarea("Ir al cine a ver la nueva película de Marvel", 3, LocalDate.now().minusDays(1));
-	        
+
 	        // Marcar la segunda tarea como completa
 	        tarea2.marcarComoCompleta();
-	        
+
+	        // Agregar las tareas a la lista
+	        listaTareas.agregar(tarea1);
+	        listaTareas.agregar(tarea2);
+	        listaTareas.agregar(tarea3);
+
 	        // Mostrar las tareas y sus estados
-	        System.out.println("Tarea 1: " + tarea1);
-	        System.out.println("Tarea 2: " + tarea2);
-	        System.out.println("Tarea 3: " + tarea3);
+	        listaTareas.imprimir();
 	    }
 	}
 
